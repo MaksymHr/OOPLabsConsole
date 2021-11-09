4c0d5210ac324e0e8c9c621cf752a4b8b9f6b593
MaksimHryschenko <st.m.hryschenko@gmail.com>
2021-11-09 19:56:03 +0200

    add lb

--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@ namespace ConsoleLabsOOP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Exercise 1.");
+            Console.Write("Input minutes: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input seconds: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Time time = new Time(a, b);
+
+            Console.WriteLine($"{time.Format()}");
+            Console.WriteLine($"We have {time.Count_of_Seconds()} seconds.");
+
+            Console.WriteLine("Exercise 2.");
+            Console.Write("Input hours: ");
+            int a0 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input minutes: ");
+            int b0 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input seconds: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            TimeEx2 time0 = new TimeEx2(a, b, c);
+
+            Console.WriteLine($"{time0.CurTime()}");
+            Console.WriteLine($"To midnight {time0.ToMidnight()} minutes.");
+
+            time0.Plus100();
+            Console.WriteLine($"{time0.CurTime()}");
+
             return;
         }
     }
