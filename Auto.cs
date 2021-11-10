b3ba333c4ec98e797cd7f9ce19f58813318a9204
MaksimHryschenko <st.m.hryschenko@gmail.com>
2021-11-10 17:06:41 +0200

    add 5-2

---
+++ Auto.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace ConsoleLabsOOP
+{
+    class Auto : IGlobal
+    {
+        double engineVolume;
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public double EngineVolume
+        {
+            get { return engineVolume; }
+            set
+            {
+                if (value < 0) engineVolume = 0.1;
+                else engineVolume = value;
+            }
+        }
+        public DateTime ReleaseDate { get; set; }
+
+        public Auto(string _brand, string _model, double _volume, DateTime _date)
+        {
+            Brand = _brand;
+            Model = _model;
+            EngineVolume = _volume;
+            ReleaseDate = _date;
+        }
+        public Auto(string _brand, string _model, double _volume, string _date)
+        {
+            Brand = _brand;
+            Model = _model;
+            EngineVolume = _volume;
+            ReleaseDate = DateTime.Parse(_date);
+        }
+        public Auto()
+        {
+            Brand = "Unknown";
+            Model = "Unknown";
+            EngineVolume = 0;
+            ReleaseDate = DateTime.MinValue;
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Please, input information for Auto:");
+            Console.Write("Brand: ");
+            this.Brand = Console.ReadLine();
+
+            Console.Write("Model: ");
+            this.Model = Console.ReadLine();
+
+            Console.Write("Engine volume: ");
+            this.EngineVolume = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Release date (d/m/y): ");
+            this.ReleaseDate = DateTime.Parse(Console.ReadLine());
+            
+            Console.WriteLine();
+
+            return; 
+        }
+        public void Output()
+        {
+            Console.WriteLine(
+                $"Main info:\n" +
+                $"Class: Auto\n" +
+                $"Brand: {this.Brand}\n" +
+                $"Model: {this.Model}\n" +
+                $"Engine volume: {this.EngineVolume} L\n" +
+                $"Release date: {this.ReleaseDate.ToLongDateString()}\n" +
+                $"Operating time: {this.Processing()} years\n"
+                );
+        }
+        public double Processing() 
+        {
+            DateTime now = DateTime.Today;
+            DateTime zeroTime = new DateTime(1, 1, 1);
+            TimeSpan span = now - ReleaseDate;
+            int years = (zeroTime + span).Year - 1;
+            return Convert.ToDouble(years);
+        }
+    }
+}
---
+++ Rent.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace ConsoleLabsOOP
+{
+    class Rent : IGlobal
+    {
+        int numberOfRooms;
+        int totalArea;
+        int price;
+        public string Address { get; set; }
+        public int NumberOfRooms
+        {
+            get { return numberOfRooms; }
+            set
+            {
+                if (value < 0) numberOfRooms = 0;
+                else numberOfRooms = value;
+            }
+        }
+        public int TotalArea
+        {
+            get { return totalArea; }
+            set
+            {
+                if (value < 0) totalArea = 0;
+                else totalArea = value;
+            }
+        }
+        public int Price
+        {
+            get { return price; }
+            set
+            {
+                if (value < 0) price = 0;
+                else price = value;
+            }
+        }
+
+        public Rent(string _address, int _rooms, int _area, int _price)
+        {
+            Address = _address;
+            NumberOfRooms = _rooms;
+            TotalArea = _area;
+            Price = _price;
+        }
+        public Rent()
+        {
+            Address = "Unknown";
+            NumberOfRooms = 0;
+            TotalArea = 0;
+            Price = 0;
+        }
+
+        public double Processing()
+        {
+            return this.Price / this.TotalArea;
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Please, input info for Rent:");
+
+            Console.Write("Appartments address: ");
+            this.Address = Console.ReadLine();
+
+            Console.Write("Number of rooms: ");
+            this.NumberOfRooms = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Total area: ");
+            this.TotalArea = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Rental cost: ");
+            this.Price = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            return;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine(
+                $"Main info:\n" +
+                $"Class: Rent\n" +
+                $"Address: {this.Address}\n" +
+                $"Number of Rooms: {this.NumberOfRooms}\n" +
+                $"Total area: {this.TotalArea} m^2\n" +
+                $"Rental const: {this.Price} hrn\n" +
+                $"Cost for m^2: {this.Processing()} hrn\n"
+                );
+            return;
+        }
+
+    }
+}
